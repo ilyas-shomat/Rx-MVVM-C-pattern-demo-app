e2855b957ab0eaa981037b1ca8a0142652bb4be9
Ilyas Shomat <ilyas.shomat@gmail.com>
2020-03-16 15:44:05 +0600

    Initial Commit

---
+++ Rx-MVVM-C Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rx-MVVM-C Test
+//
+//  Created by Ilyas Shomat on 3/16/20.
+//  Copyright © 2020 Ilyas Shomat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
